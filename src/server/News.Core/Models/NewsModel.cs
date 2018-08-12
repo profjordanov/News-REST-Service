ee1d02743c9ee2d4a63314fe828a47d91af6dbe2
Jordan Jordanov <d_yugioh@abv.bg>
2018-08-12 13:13:25 +0300

    Model

---
+++ src/server/News.Core/Models/NewsModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace News.Core.Models
+{
+    public class NewsModel
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public DateTime PublishDate { get; set; } = DateTime.Now;
+    }
+}
