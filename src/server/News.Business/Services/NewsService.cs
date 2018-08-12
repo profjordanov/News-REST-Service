5a8d0f5aa1edc6e40d2bed0fe776978e4f285165
Jordan Jordanov <d_yugioh@abv.bg>
2018-08-12 13:24:46 +0300

    Exists methods

--- src/server/News.Business/Services/NewsService.cs
+++ src/server/News.Business/Services/NewsService.cs
@@ -1,9 +1,31 @@
-﻿using News.Core.Services;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using News.Core.Services;
+using News.Data.EntityFramework;
+using Optional;
 
 namespace News.Business.Services
 {
     public class NewsService : INewsService
     {
-        
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public NewsService(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        private async Task<bool> ExistsById(int id)
+            => await _applicationDbContext
+                .News
+                .Where(news => news.Id == id)
+                .AnyAsync();
+
+        private async Task<bool> ExistsByTitle(string title)
+            => await _applicationDbContext
+                .News
+                .Where(news => news.Title == title)
+                .AnyAsync();
     }
 }
