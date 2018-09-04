a2042dbde1a0efe2c50de7639df4282d6203c804
Jordan Jordanov <d_yugioh@abv.bg>
2018-09-04 22:47:54 +0300

    Blazor Services Ready

--- News.Client/Extensions/HttpClientExtensions.cs
+++ News.Client/Extensions/HttpClientExtensions.cs
@@ -5,6 +5,7 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using News.Client.Data;
 using News.Client.Models;
+using Newtonsoft.Json;
 
 namespace News.Client.Extensions
 {
@@ -16,25 +17,24 @@ namespace News.Client.Extensions
         {
             try
             {
-                //var response = await httpClient.GetAsync(uri + queryString);
-                //TResponse resultResponse = default(TResponse);
-                //Error error = null;
-                //if (response.IsSuccessStatusCode)
-                //{
-                //    resultResponse = JsonConvert.DeserializeObject<TResponse>(await result.Content.ReadAsStringAsync(););
-                //}
-                //else
-                //{
-                //    error = JsonConvert.DeserializeObject<Error>(await result.Content.ReadAsStringAsync());
-                //}
-                //return new ApiObjectResponse<TObject>
-                //{
-                //    Error = error,
-                //    Response = resultResponse,
-                //    StatusCode = response.StatusCode
-                //};
+                var response = await httpClient.GetAsync(uri + queryString);
+                TResponse resultResponse = default;
+                Error error = null;
+                if (response.IsSuccessStatusCode)
+                {
+                    resultResponse = JsonConvert.DeserializeObject<TResponse>(await response.Content.ReadAsStringAsync());
+                }
+                else
+                {
+                    error = JsonConvert.DeserializeObject<Error>(await response.Content.ReadAsStringAsync());
+                }
+                return new ApiObjectResponse<TResponse>
+                {
+                    Error = error,
+                    Response = resultResponse,
+                    StatusCode = response.StatusCode
+                };
 
-                return new ApiObjectResponse<TResponse>();
             }
             catch (Exception e)
             {
@@ -49,25 +49,24 @@ namespace News.Client.Extensions
         {
             try
             {
-                //var response = await httpClient.GetAsync(uri);
-                //TResponse resultResponse = default(TResponse);
-                //Error error = null;
-                //if (response.IsSuccessStatusCode)
-                //{
-                //    resultResponse = JsonConvert.DeserializeObject<TResponse>(await result.Content.ReadAsStringAsync(););
-                //}
-                //else
-                //{
-                //    error = JsonConvert.DeserializeObject<Error>(await result.Content.ReadAsStringAsync());
-                //}
-                //return new ApiObjectResponse<TObject>
-                //{
-                //    Error = error,
-                //    Response = resultResponse,
-                //    StatusCode = response.StatusCode
-                //};
+                var response = await httpClient.GetAsync(uri);
+                TResponse resultResponse = default;
+                Error error = null;
+                if (response.IsSuccessStatusCode)
+                {
+                    resultResponse = JsonConvert.DeserializeObject<TResponse>(await response.Content.ReadAsStringAsync());
+                }
+                else
+                {
+                    error = JsonConvert.DeserializeObject<Error>(await response.Content.ReadAsStringAsync());
+                }
+                return new ApiObjectResponse<TResponse>
+                {
+                    Error = error,
+                    Response = resultResponse,
+                    StatusCode = response.StatusCode
+                };
 
-                return new ApiObjectResponse<TResponse>();
             }
             catch (Exception e)
             {
@@ -80,84 +79,80 @@ namespace News.Client.Extensions
             this HttpApiClient httpClient, string uri, TRequest content)
             where TResponse : new()
         {
-            //var myContent = JsonConvert.SerializeObject(content);
-            //var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
-            //var byteContent = new ByteArrayContent(buffer);
+            var myContent = JsonConvert.SerializeObject(content);
+            var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
+            var byteContent = new ByteArrayContent(buffer);
 
-            //byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-            //byteContent.Headers.ContentType.Parameters.Add(new NameValueHeaderValue("charset", "utf-8"));
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            byteContent.Headers.ContentType.Parameters.Add(new NameValueHeaderValue("charset", "utf-8"));
 
-            //var result = await httpClient.PostAsync(uri, byteContent);
+            var result = await httpClient.PostAsync(uri, byteContent);
 
-            //TResponse resultResponse = default;
-            //Error error = null;
+            TResponse resultResponse = default;
+            Error error = null;
 
-            //if (result.IsSuccessStatusCode)
-            //{
-            //    var a = await result.Content.ReadAsStringAsync();
-            //    resultResponse = JsonConvert.DeserializeObject<TResponse>(a);
-            //}
-            //else
-            //{
-            //    error = JsonConvert.DeserializeObject<Error>(await result.Content.ReadAsStringAsync()); ;
-            //}
+            if (result.IsSuccessStatusCode)
+            {
+                var a = await result.Content.ReadAsStringAsync();
+                resultResponse = JsonConvert.DeserializeObject<TResponse>(a);
+            }
+            else
+            {
+                error = JsonConvert.DeserializeObject<Error>(await result.Content.ReadAsStringAsync()); ;
+            }
 
-            //return new ApiObjectResponse<TResponse>
-            //{
-            //    Error = error,
-            //    Response = resultResponse,
-            //    StatusCode = result.StatusCode
-            //};
+            return new ApiObjectResponse<TResponse>
+            {
+                Error = error,
+                Response = resultResponse,
+                StatusCode = result.StatusCode
+            };
 
-            return new ApiObjectResponse<TResponse>();
         }
 
         public static async Task<ApiResponse> UpdateAsync(
             this HttpApiClient httpClient, string requestUri, object content)
         {
-            //var myContent = JsonConvert.SerializeObject(content);
-			//var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
-			//var byteContent = new ByteArrayContent(buffer);
+            var myContent = JsonConvert.SerializeObject(content);
+            var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
+            var byteContent = new ByteArrayContent(buffer);
 
-			//byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-			//byteContent.Headers.ContentType.Parameters.Add(new NameValueHeaderValue("charset", "utf-8"));
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            byteContent.Headers.ContentType.Parameters.Add(new NameValueHeaderValue("charset", "utf-8"));
 
-			//var result = await httpClient.PutAsync(requestUri, byteContent);
+            var result = await httpClient.PutAsync(requestUri, byteContent);
 
-			//Error error = null;
+            Error error = null;
 
-			//if (!result.IsSuccessStatusCode)
-			//{
-			//	error = JsonConvert.DeserializeObject<Error>(await result.Content.ReadAsStringAsync());;
-			//}
+            if (!result.IsSuccessStatusCode)
+            {
+                error = JsonConvert.DeserializeObject<Error>(await result.Content.ReadAsStringAsync()); ;
+            }
 
-			//return new ApiResponse
-			//{
-			//	Error = error,
-			//	StatusCode = result.StatusCode
-			//};
-
-            return new ApiResponse();
+            return new ApiResponse
+            {
+                Error = error,
+                StatusCode = result.StatusCode
+            };
         }
 
         public static async Task<ApiResponse> DeleteAsync(this HttpClient httpClient, string requestUri)
         {
-            //var result = await httpClient.DeleteAsync(requestUri);
+            var result = await httpClient.DeleteAsync(requestUri);
 
-            //Error error = null;
+            Error error = null;
 
-            //if (!result.IsSuccessStatusCode)
-            //{
-            //    error = JsonConvert.DeserializeObject<Error>(await result.Content.ReadAsStringAsync()); ;
-            //}
+            if (!result.IsSuccessStatusCode)
+            {
+                error = JsonConvert.DeserializeObject<Error>(await result.Content.ReadAsStringAsync()); ;
+            }
 
-            //return new ApiResponse
-            //{
-            //    Error = error,
-            //    StatusCode = result.StatusCode
-            //};
+            return new ApiResponse
+            {
+                Error = error,
+                StatusCode = result.StatusCode
+            };
 
-            return new ApiResponse();
         }
     }
 }
