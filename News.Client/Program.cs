479f2d19f4463fd5922d28013486d0c105a549c7
Jordan Jordanov <d_yugioh@abv.bg>
2018-09-08 12:44:16 +0300

    Blazor changes

--- News.Client/Program.cs
+++ News.Client/Program.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Blazor.Hosting;
+﻿using Microsoft.AspNetCore.Blazor.Browser.Rendering;
+using Microsoft.AspNetCore.Blazor.Browser.Services;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using News.Client.Data;
+using News.Client.Services;
 
 namespace News.Client
 {
@@ -6,7 +11,18 @@ namespace News.Client
     {
         public static void Main(string[] args)
         {
+
+            var serviceProvider = new BrowserServiceProvider(services =>
+            {
+                services.AddScoped<HttpApiClient>();
+
+                services.AddSingleton<NewsServices>();
+            });
+
+            new BrowserRenderer(serviceProvider).AddComponent<App>("app");
+
             CreateHostBuilder(args).Build().Run();
+
         }
 
         public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
