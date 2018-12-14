a28da695848395f829dbaad0dfb2961b47b88ca4
Jordan Jordanov <d_yugioh@abv.bg>
2018-12-14 18:56:50 +0200

    mvc client app added

---
+++ src/clients/News.Mvc.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace News.Mvc.Client.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
